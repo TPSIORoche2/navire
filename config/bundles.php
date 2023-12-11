00ec281a1828bc588f2b9dfa152226b50e38765d
matis.dembele <matis.dembele@CP-SIO-W11-PG07.bts.sio>
2023-12-11 10:42:57 +0100

    Dependences: 	Ajout de symfony/orm-pack -w

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
